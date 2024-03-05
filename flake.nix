c2962c84bb8ce9b677abb9b5e9306e815db8405d
Walter Moreira <walter@waltermoreira.net>
2024-03-04 23:02:49 -0600

    Add flake

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        prev = pkgs.stdenv.mkDerivation {
+          name = "test";
+          src = pkgs.fetchFromGitHub {
+            owner = "waltermoreira";
+            repo = "nested-fetch";
+            rev = "5fea38724a96da6290cf85923f2153ad2c519dac";
+            hash = "sha256-gC8yRUJAFPbobvI/W/OIfmhzu1Bb/5ns78crEaJ3zEA=";
+          };
+          installPhase = ''
+            mkdir -p $out
+            cp README.md $out
+          '';
+        };
+      in
+      {
+        packages.default = prev;
+      });
+}
